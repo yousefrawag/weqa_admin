952af417ff8d61cb780e908b0fb6ac4091b05604
yousefrawag <yousefrawag908@gmail.com>
2025-01-01 11:08:11 +0200

    v1.1

--- src/components/common/popupmdules/MdouleAddCategoray.jsx
+++ src/components/common/popupmdules/MdouleAddCategoray.jsx
@@ -29,6 +29,7 @@ const MdouleAddCategoray = () => {
           {
             Levels.map((item) => {
               return    <button
+              key={item}
               onClick={() => SetCurrentLevel(item)}
               className={`block text-white  ${Currentlevel === item ? "bg-main2" :"bg-main"}  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  dark:focus:ring-blue-800`}
               type="button"
